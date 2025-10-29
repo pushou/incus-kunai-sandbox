1fcc11ba74d04ba8a8c9f28e2c7c7fcff161171c
jean-marc pouchoulon <pouchougpro@gmail.com>
2025-10-29 12:35:46 +0100

    all in one script

--- install-all.sh
+++ install-all.sh
@@ -1,5 +1,5 @@
-# goal
-provide kunai-sandbox as a nested VM  in an debian Incus  VM
+#/bin/bash
+# all in one install
 
 ## install incus on your host
 ./install-incus.sh
@@ -9,4 +9,5 @@ incus profile create kunai-sandbox < kunai-sandbox.profile
 
 ## launch VM
 # (16GiB is comfortable you can try with 8GiB if you have not sufficient resources)
+# if incus is already installed change storage with --storage
 incus launch images:debian/13/cloud sandbox-vm --vm --profile kunai-sandbox -c limits.cpu=2 -c limits.memory=16GiB
